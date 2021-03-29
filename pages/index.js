00cd8b2bd58052d57046f110930e1644eb4dc63e
habib610 <habib54562@gmail.com>
2021-03-29 23:35:18 +0600

    wrapping-with-global-components

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,8 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div>
-      <Header />
       <h1>home page</h1>
-
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid repellendus ea, voluptate laborum eligendi voluptates sed reprehenderit tempore inventore, fugiat, nulla ipsum dolor ipsa molestiae doloremque distinctio consectetur ex accusamus! Iure similique asperiores adipisci nostrum suscipit soluta itaque, iste dolorum! Ratione, culpa, corrupti fuga neque molestiae sunt alias debitis, perferendis nostrum impedit ducimus. Pariatur ipsam numquam quis corporis ad minus quo molestiae quia esse. Illo amet cum maiores sunt nihil consectetur esse facilis, beatae exercitationem? Distinctio magni corporis ipsa harum nobis ratione ea incidunt ipsam ullam nihil voluptatum, neque recusandae debitis sapiente expedita, porro facere soluta quibusdam quos, at rem?</p>
-      <Footer />
     </div>
   )
 }
