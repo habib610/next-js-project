ce352ccfe5eec792b8169e9b1330f9765a2ac9fe
habib610 <habib54562@gmail.com>
2021-03-30 00:37:37 +0600

    redirects-user

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,15 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect } from 'react';
+import {useRouter} from 'next/router'
 
 const NotFound = () => {
+    const router =  useRouter()
+    
+    useEffect(()=> {
+        setTimeout(()=> {
+        router.push('/')
+        }, 1000)
+    }, [])
     return (
         <div className="not-found">
             <h1>Oppsss....</h1>
