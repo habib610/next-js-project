4d3bda820e3c2cc02c3726f8367184305245eece
habib610 <habib54562@gmail.com>
2021-03-30 01:29:17 +0600

    getStaticProps

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,22 +1,32 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import React from 'react';
+import React from 'react'; 
+import styles from '../../styles/Jobs.module.css'
 
-const Ninjas = () => {
+export const getStaticProps = async()=> {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await res.json()
+
+    return {
+        props: { ninjas: data}
+    }
+}
+const Ninjas = ({ninjas}) => {
     return (
         <>
         <Head>
             <title>Ninjas | All Ninjas</title>
         </Head>
-        <div>
-            <h1>All Ninjas</h1>
-            <p>Lorem ipsum dolor sit amet.</p>
-            <p>Lorem ipsum dolor sit amet.</p>
-            <p>Lorem ipsum dolor sit amet.</p>
-            <p>Lorem ipsum dolor sit amet.</p>
-            <p>Lorem ipsum dolor sit amet.</p>
+        {
+            ninjas.map(ninja => (
+                <div className={styles.single} key={ninja.id}>
+                    <a >
+                        <h3>{ninja.name}</h3>
+                    </a>
+                </div>
+            ))
+        }
             <Link href="/"><a>Home Page</a></Link>
-        </div>
         </>
     );
 };
