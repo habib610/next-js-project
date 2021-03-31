c526ad57d8479820d287e2e2a591231e8e150a16
habib610 <habib54562@gmail.com>
2021-03-31 23:46:57 +0600

    dynamic-route

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -3,29 +3,43 @@ import Link from 'next/link';
 import React from 'react'; 
 import styles from '../../styles/Jobs.module.css'
 
-export const getStaticProps = async()=> {
-    const res = await fetch('https://jsonplaceholder.typicode.com/users')
-    const data = await res.json()
+// export const getStaticProps = async()=> {
+//     const res = await fetch('https://restcountries.eu/rest/v2/all')
+//     const data = await res.json()
+//     console.log(data)
 
-    return {
-        props: { ninjas: data}
-    }
-}
-const Ninjas = ({ninjas}) => {
+//     return {
+//         props: { ninjas: data}
+//     }
+// }
+const Ninjas = () => {
+    
     return (
         <>
         <Head>
             <title>Ninjas | All Ninjas</title>
         </Head>
-        {
-            ninjas.map(ninja => (
-                <div className={styles.single} key={ninja.id}>
+        {/* {
+            ninjas.map((ninja, index) => (
+                <div className={styles.single} key={index}>
                     <a >
                         <h3>{ninja.name}</h3>
                     </a>
                 </div>
             ))
-        }
+        }  */}
+        <Link href="/ninjas/1">
+            <a>Ninja one</a>
+        </Link>
+        <p></p>
+        <Link href="/ninjas/2">
+            <a>Ninja one</a>
+        </Link>
+        <p></p>
+        <Link href='/ninjas/3'>
+            <a>Ninja one</a>
+        </Link>
+     <p></p>
             <Link href="/"><a>Home Page</a></Link>
         </>
     );
