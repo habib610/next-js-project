10f7493b299f85aa033cfe31ae01575496a7f47b
habib610 <habib54562@gmail.com>
2021-04-04 02:10:50 +0600

    get-single-data

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,5 +1,5 @@
 import React from 'react';
-const getStaticPaths = async()=> {
+export const getStaticPaths = async()=> {
     const res = await fetch('https://jsonplaceholder.typicode.com/users')
     const data = await res.json()
 
@@ -14,10 +14,24 @@ const getStaticPaths = async()=> {
     }
 }
 
-const Details = () => {
+ export const getStaticProps = async (context) => {
+    const id = context.params.id 
+    
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+    const data = await res.json()
+    
+    return {
+        props: {ninja: data}
+    }
+}
+
+const Details = ({ninja}) => {
     return (
         <div>
-            <h1>Details Page</h1>
+            <h1>{ ninja.name }</h1>
+            <p>{ ninja.email }</p>
+            <p>{ninja.website}</p>
+            <p>{ninja.address.street}</p>
         </div>
     );
 };
