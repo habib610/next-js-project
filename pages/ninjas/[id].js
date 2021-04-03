64bb08ac23b9be0e3c129a616f1981b1dbb986d9
habib610 <habib54562@gmail.com>
2021-04-04 01:51:26 +0600

    getStaticPaths

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,4 +1,18 @@
 import React from 'react';
+const getStaticPaths = async()=> {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await res.json()
+
+    const paths = data.map(ninja => {
+        return {
+            params: {id: ninja.id.toString()}
+        }
+    })
+    return {
+        ninja,
+        fallback: false
+    }
+}
 
 const Details = () => {
     return (
--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -3,31 +3,32 @@ import Link from 'next/link';
 import React from 'react'; 
 import styles from '../../styles/Jobs.module.css'
 
-// export const getStaticProps = async()=> {
-//     const res = await fetch('https://restcountries.eu/rest/v2/all')
-//     const data = await res.json()
-//     console.log(data)
+export const getStaticProps = async()=> {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await res.json()
+    console.log(data)
+ 
 
-//     return {
-//         props: { ninjas: data}
-//     }
-// }
-const Ninjas = () => {
+    return {
+        props: { ninjas: data}
+    }
+}
+const Ninjas = ({ninjas}) => {
     
     return (
         <>
         <Head>
             <title>Ninjas | All Ninjas</title>
         </Head>
-        {/* {
-            ninjas.map((ninja, index) => (
-                <div className={styles.single} key={index}>
-                    <a >
+        {
+           ninjas.map((ninja, index) => (
+                <Link  href={`/ninjas/${ninja.id}`} key={index}>
+                    <a className={styles.single}>
                         <h3>{ninja.name}</h3>
                     </a>
-                </div>
+                </Link>
             ))
-        }  */}
+        } 
         <Link href="/ninjas/1">
             <a>Ninja one</a>
         </Link>
